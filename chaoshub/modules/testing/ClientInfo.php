ba09dbc0e2225fdc81a379241b9bb87197c066ec
hal9000 <halcyon.cybernetics@gmail.com>
2024-10-02 07:56:58 +0900

    execute order-66

--- chaoshub/modules/testing/ClientInfo.php
+++ chaoshub/modules/testing/ClientInfo.php
@@ -16,13 +16,23 @@ See the License for the specific language governing permissions and
 limitations under the License.
 **************************************************************************/
 
-class Info  {
+class ClientInfo  {
 
     public function __construct(?array $permissions) {
     }
 
     public function respond() {
-        phpinfo() ;
+        $headers = apache_request_headers();
+        $browserData = get_browser(null, true);
+        echo "<table border='1' cellpadding='5' cellspacing='0'>";
+        echo "<tr><th>Key</th><th>Value</th></tr>";
+        foreach ($browserData as $key => $value) {
+            echo "<tr><td>{$key}</td><td>" . htmlspecialchars($value, ENT_QUOTES, 'UTF-8') . "</td></tr>";
+        }
+        foreach ($headers as $key => $value) {
+            echo "<tr><td>{$key}</td><td>{$value}</td></tr>";
+        }
+        echo "</table>";
         exit ;
     }
     public function setClient($client){}
